2fc74a70d3bcf03461dd2b2f16fd086744c468bc
Andrii <ifromukraine2022@gmail.com>
2023-09-10 21:25:12 +0300

    add readme

--- src/components/ModalTeacher/ModalTeacher.jsx
+++ src/components/ModalTeacher/ModalTeacher.jsx
@@ -10,7 +10,7 @@ import {
   Img,
   ImgText,
   WrapImg,
-  WrapModal,
+  WrapModal
 } from './ModalTeacher.styled';
 import { MarkupButton } from 'components/MarkupButton/MarkupButton';
 
@@ -19,65 +19,64 @@ export const ModalTeacher = ({ closeModal, avatar, name, surname }) => {
     <BackdropModal closeModal={closeModal}>
       <WrapModal>
         <Modal>
-          <Title>Book trial lesson</Title>
-          <SecondText>
-            Our experienced tutor will assess your current language level,
-            discuss your learning goals, and tailor the lesson to your specific
-            needs.
-          </SecondText>
-          <WrapImg>
-            <Img src={avatar} />
-            <div>
-              <ImgText>Your teacher</ImgText>
-              <p>
-                {name} {surname}
-              </p>
-            </div>
-          </WrapImg>
-          <form>
-            <SecondTitle>
-              What is your main reason for learning English?
-            </SecondTitle>
-            <WrapRadioInput>
-              <label>
-                <input type="radio" name="options" value="option1" />
-                Career and business
-              </label>
+        <Title>Book trial lesson</Title>
+        <SecondText>
+          Our experienced tutor will assess your current language level, discuss
+          your learning goals, and tailor the lesson to your specific needs.
+        </SecondText>
+        <WrapImg>
+          <Img src={avatar} />
+          <div>
+            <ImgText>Your teacher</ImgText>
+            <p>
+              {name} {surname}
+            </p>
+          </div>
+        </WrapImg>
+        <form>
+          <SecondTitle>
+            What is your main reason for learning English?
+          </SecondTitle>
+          <WrapRadioInput>
+            <label>
+              <input type="radio" name="options" value="option1" />
+              Career and business
+            </label>
 
-              <label>
-                <input type="radio" name="options" value="option2" />
-                Lesson for kids
-              </label>
+            <label>
+              <input type="radio" name="options" value="option2" />
+              Lesson for kids
+            </label>
 
-              <label>
-                <input type="radio" name="options" value="option3" />
-                Living abroad
-              </label>
+            <label>
+              <input type="radio" name="options" value="option3" />
+              Living abroad
+            </label>
 
-              <label>
-                <input type="radio" name="options" value="option4" />
-                Exams and coursework
-              </label>
+            <label>
+              <input type="radio" name="options" value="option4" />
+              Exams and coursework
+            </label>
 
-              <label>
-                <input type="radio" name="options" value="option5" />
-                Culture, travel or hobby
-              </label>
-            </WrapRadioInput>
-            <WrapInput>
-              <label>
-                <Input type="text" placeholder="Full Name" />
-              </label>
-              <label>
-                <Input type="text" placeholder="Email" />
-              </label>
-              <label>
-                <Input type="text" placeholder="Phone number" />
-              </label>
-            </WrapInput>
-            <MarkupButton title="Book" width="100%" />
-          </form>
-        </Modal>
+            <label>
+              <input type="radio" name="options" value="option5" />
+              Culture, travel or hobby
+            </label>
+          </WrapRadioInput>
+          <WrapInput>
+            <label>
+              <Input type="text" placeholder="Full Name" />
+            </label>
+            <label>
+              <Input type="text" placeholder="Email" />
+            </label>
+            <label>
+              <Input type="text" placeholder="Phone number" />
+            </label>
+          </WrapInput>
+          <MarkupButton title="Book" width="100%" />
+        </form>
+      </Modal>
       </WrapModal>
     </BackdropModal>
   );
